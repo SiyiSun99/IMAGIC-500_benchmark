b14b66e8e4bb1bfa7b74c1e677bf40f92dc7b5b0
Siyi Sun <98569478+SiyiSun99@users.noreply.github.com>
2025-05-12 03:16:01 +0100

    Update softimpute_impute.py

--- Python_code/softimpute_impute.py
+++ Python_code/softimpute_impute.py
@@ -7,26 +7,12 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from hyperimpute.utils.benchmarks import compare_models
 from tqdm import tqdm
 
-
-def round_to_nearest_half(value):
-    """
-    Rounds a float value to the nearest 0.5
-
-    Args:
-        value (float): The value to round
-
-    Returns:
-        float: The value rounded to the nearest 0.5
-    """
-    return round(value * 2) / 2
-
-
 ## NEED TO CHANGE THE PATH BASED ON YOUR COMPUTER ##
 cohorts = ["SynthSurvey"]
 
 base_path = Path("/Users/siysun/Desktop/NeurIPS25/data_stored")
 output_file = Path(
- f"/Users/siysun/Desktop/NeurIPS25/imputation_times_mean_mode_{cohorts[0]}.csv"
+ f"/Users/siysun/Desktop/NeurIPS25/imputation_times_softimpute_{cohorts[0]}.csv"
 )
 methods = ["MCAR", "MAR", "MNAR"]
 ratios = [10, 20, 30, 40, 50]
@@ -46,7 +32,7 @@ test_full = pd.read_csv(full_test_path)
 # encode categorical feature
 encoders = {}
 for col in train_full.columns:
-    if col.startswith("cat_"):
+    if col.startswith("cat_") and col != "cat_hid":
         tem_encoder = LabelEncoder()
         train_full[col] = tem_encoder.fit_transform(train_full[col])
         test_full[col] = tem_encoder.transform(test_full[col])
@@ -96,12 +82,12 @@ for method in tqdm(methods):
 
             # decode categorical
             for col in out_train.columns:
-                if col.startswith("cat_"):
+                if col == "cat_hid":
+                    pass
+                elif col.startswith("cat_"):
                     tem_encoder = encoders[col]
                     out_train[col] = (out_train[col] > 0.5).astype(int)
                     out_train[col] = tem_encoder.inverse_transform(out_train[col])
-                elif col.startswith("con_TS_ON"):
-                    out_train[col] = round_to_nearest_half(out_train[col])
                 else:
                     out_train[col] = round(out_train[col])
 
@@ -118,12 +104,12 @@ for method in tqdm(methods):
 
             # decode categorical
             for col in out_test.columns:
-                if col.startswith("cat_"):
+                if col == "cat_hid":
+                    pass
+                elif col.startswith("cat_"):
                     tem_encoder = encoders[col]
                     out_test[col] = (out_test[col] > 0.5).astype(int)
                     out_test[col] = tem_encoder.inverse_transform(out_test[col])
-                elif col.startswith("con_TS_ON"):
-                    out_test[col] = round_to_nearest_half(out_test[col])
                 else:
                     out_test[col] = round(out_test[col])
 
