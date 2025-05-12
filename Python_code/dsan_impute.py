657b59be4147ede944bad012368d1189f82fca04
Han DC <43898071+hyc1998@users.noreply.github.com>
2025-05-12 02:53:18 +0100

    Update dsan_impute.py

--- Python_code/dsan_impute.py
+++ Python_code/dsan_impute.py
@@ -657,21 +657,20 @@ class MVImputer(object):
 
 if __name__ == "__main__":
     # Define cohorts, missing methods, and ratios
-    cohorts = ["C19"]
+    cohorts = ["SynthSurvey"]
     missing_methods = ["MCAR", "MAR", "MNAR"]
     missing_ratios = [10, 20, 30, 40, 50]
     SampleTime = 5
     ## NEED TO CHANGE THE PATH BASED ON YOUR COMPUTER ##
-    base_path = Path("/Users/siysun/Desktop/PhD/SynthCPHS_benchmark/data_stored")
+    base_path = Path("/Users/siysun/Desktop/NeurIPS25/data_stored")
     impute_method = "dsan"
 
     ## NEED TO CHANGE THE PATH BASED ON YOUR COMPUTER ##
     # Output path for time recording
     output_file = Path(
-        "/Users/siysun/Desktop/PhD/SynthCPHS_benchmark/imputation_times_dsan_C19_synthetic.csv"
+     f"/Users/siysun/Desktop/NeurIPS25/imputation_times_mean_mode_{cohorts[0]}.csv"
     )
     time_records = []
-
     for cohort in cohorts:
         full_path = base_path / f"Completed_data/{cohort}/{cohort}_train.csv"
         full_data = pd.read_csv(full_path, header=0)
